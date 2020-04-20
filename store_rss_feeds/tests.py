848af7bc0e1137f458d60f6dce18a02f9ed0735c
Gideon Maina <maina.gidi@gmail.com>
2020-04-20 20:38:26 +0300

    task: - Add storage of feeds tests - Add viewing of articles tests

--- store_rss_feeds/tests.py
+++ store_rss_feeds/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import os
 
-# Create your tests here.
+from django.test import TestCase, Client
+from store_rss_feeds.models import RSSSource
+from store_rss_feeds.forms import RSSSourcesFileForm
+
+
+class TestRSSSourcesStorage(TestCase):
+    def setUp(self):
+        self.current_path = os.path.dirname(os.path.realpath(__file__))
+        self.sample_file = os.path.join(self.current_path,
+                                        'sample-upload-test.csv')
+        self.store_url = '/store-rss-feeds/store'
+        self.client = Client()
+
+    def test_view_index_page(self):
+        r = self.client.get('')
+        self.assertEqual(200, r.status_code)
+
+    def test_source_file_upload(self):
+        with open(self.sample_file) as fh:
+            params = {'source_file': fh}
+            r = self.client.post(self.store_url, params)
+            self.assertEqual(r.status_code, 302)
+
+    def test_create_sources_from_upload(self):
+        expected_created_rss_source = RSSSource(
+            publisher='cnn',
+            url='http://rss.cnn.com/rss/edition_motorsport.rss')
+
+        with open(self.sample_file) as fh:
+            params = {'source_file': fh}
+            r = self.client.post(self.store_url, params)
+            self.assertEqual(302, r.status_code)
+            # Fetch all the current sources
+            source = RSSSource.objects.first()
+            self.assertEqual(expected_created_rss_source.url.strip(),
+                             source.url.strip())
--- view_rss_feeds/tests.py
+++ view_rss_feeds/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import datetime
+import os
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from .views import ArticleListView
+from store_rss_feeds.models import RSSSource, RSSFeedArticle
+
+
+class ArticlesListTest(TestCase):
+    def setUp(self):
+        self.current_path = os.path.dirname(os.path.realpath(__file__))
+        self.sample_file = os.path.join(self.current_path,
+                                        'sample-upload-test.csv')
+        self.url = '/view-rss-feeds/'
+        self.client = Client()
+        # Create dummy articles
+        source = RSSSource.objects.create(publisher='bbc',
+                                          url='http://dummy-url.com.xml')
+        # Create 12 dummy articles so that pagination activates
+        for i in range(12):
+            article = RSSFeedArticle.objects.create(
+                rss_source=source,
+                title=f"title {i}",
+                description="Description",
+                link="https://hello-dummy.article.com",
+                guid="234",
+                pubdate=datetime.datetime.now())
+
+    def test_articles_viewing(self):
+        r = self.client.get(self.url)
+        self.assertEqual(200, r.status_code)
+
+    def test_pagination_items(self):
+        r = self.client.get(self.url)
+        self.assertTrue('is_paginated' in r.context)
+        self.assertTrue(r.context['is_paginated'] == True)
+        self.assertTrue(len(r.context['object_list']) == 10)
